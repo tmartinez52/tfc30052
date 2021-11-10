2b1c2452315723bf829aad5815a54707c3d12085
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-11-10 16:20:03 -0600

    adding  main.tf file
    Creating and Adding tf File

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project = var.prject
+  region =  var.region
+  zone = var.zone
+}
+
+resource "google_project_service" "cloud_build" {
+  service = "cloudbuild.googleapis.com"
+
+  timeouts {
+    create = "20m"
+    update = "5m"
+  }
+
+  disable_dependent_services = true
+}
