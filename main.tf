38cde4ff3148bd009291342ca0cdd3883d527d7c
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2021-11-10 17:36:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,16 +4,6 @@ provider "google" {
   zone = var.zone
 }
 
-resource "google_project_service" "cloud_resources" {
-  project = var.project
-  service = "cloudresourcemanager.googleapis.com"
-
-  timeouts {
-    create = "20m"
-    update = "5m"
-  }
-}
-
 resource "google_project_service" "cloudbuilder" {
   project = var.project
   service = "cloudbuild.googleapis.com"
